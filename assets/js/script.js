8204c08c158bd528e00647fa3ccee9d5754d0953
lshillman <lshillman@users.noreply.github.com>
2022-06-27 14:51:38 -0700

    remove startbutton when it's clicked, add words array, add displayBlanks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,53 @@
 console.log("I'm a JavaScript file linked to this page!");
+
+
+// As a user, I want to start the game by clicking on a button. 
+    // get the id of the button and add an eventlistener
+var startGameBtn = document.getElementById("startGameBtn");
+startGameBtn.addEventListener("click", startGame);
+
+var randomWord;
+
+function startGame() {
+    console.log("Start game was clicked!");
+    startGameBtn.setAttribute("style", "display:none;")
+}
+
+    // have an array of some words.
+var words = ["supercalifragilisticexpialidocious", "nitwit", "blubber", "oddment", "tweak", "bulbous", "bouffant", "mukluks", "macadamia"];
+
+    // select a word from the array of words at random
+function chooseWord() {
+    randomWord = words[Math.floor(Math.random() * words.length)];
+}
+
+    // display blanks to the user for each letter of the selected word
+function displayBlanks() {
+    var blanks;
+    for (i=0; i < randomWord.length; i++) {
+        blanks += "_ "
+    }
+
+}
+
+
+
+// As a user, I want to try and guess a word by filling in a number of blanks that match the number of letters in that word.
+
+
+    // add an eventlistener to the window to listen for keypresses
+    // replace blanks with correct guesses
+    // if we were nice: keep track of incorrect guesses
+
+// As a user, I want the game to be timed
+    // setInterval thing
+
+// As a user, I want to win the game when I have guessed all the letters in the word.
+    // if all blanks have been replaced, AND there is time left, I win
+
+// As a user, I want to lose the game when the timer runs out before I have guessed all the letters.
+    // if blanks remain when timer reaches 0, I lose
+
+// As a user, I want to see my total wins and losses on the screen.
+    // at the end of each game, increment wins or losses, and display to user via "wins" and "losses" in the page footer
+
